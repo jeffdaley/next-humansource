1828a04a705af4aba4dfea357dc151550866ca0e
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-05-13 21:47:11 -0400

    Add icons

--- src/app/_components/employees/employee/edit-photo-menu.tsx
+++ src/app/_components/employees/employee/edit-photo-menu.tsx
@@ -6,6 +6,37 @@ import Action from "../../action";
 
 interface EditPhotoMenuArgs {}
 
+function ReplaceImageIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M4 5h13v7h2V5c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h8v-2H4V5z"></path>
+      <path d="m8 11-3 4h11l-4-6-3 4z"></path>
+      <path d="M19 14h-2v3h-3v2h3v3h2v-3h3v-2h-3z"></path>
+    </svg>
+  );
+}
+
+function RemoveIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M7 11h10v2H7z"></path>
+      <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
+    </svg>
+  );
+}
+
 export default function EditPhotoMenu(args: EditPhotoMenuArgs) {
   const [isShown, setIsShown] = useState(false);
 
@@ -20,9 +51,13 @@ export default function EditPhotoMenu(args: EditPhotoMenuArgs) {
         Edit photo <span className="ml-1">▾</span>
       </Action>
       {isShown && (
-        <div className="absolute -bottom-1 right-0 flex w-56 translate-y-full flex-col rounded-3xl bg-white py-3 text-black">
-          <Action className="px-6 py-0.5 text-left">Replace with...</Action>
-          <Action className="px-6 py-0.5 text-left text-pink-600">
+        <div className="absolute -bottom-1 right-0 flex w-56 translate-y-full flex-col overflow-hidden rounded-3xl bg-white py-3 text-black">
+          <Action className="flex gap-2 px-6 py-1 text-left hover:bg-neutral-200">
+            <ReplaceImageIcon />
+            Replace...
+          </Action>
+          <Action className="flex gap-2 px-6 py-1 text-left text-pink-600 hover:bg-neutral-200">
+            <RemoveIcon />
             Remove
           </Action>
         </div>
--- src/app/_components/route-nav.tsx
+++ src/app/_components/route-nav.tsx
@@ -6,30 +6,88 @@ import { usePathname } from "next/navigation";
 export type NavLink = {
   href: string;
   text: string;
-  icon: string;
+  icon: JSX.Element;
 };
+
+function HomeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm7 7v-5h4v5h-4zm2-15.586 6 6V15l.001 5H16v-5c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v5H6v-9.586l6-6z"></path>
+    </svg>
+  );
+}
+
+function PeopleIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M16.604 11.048a5.67 5.67 0 0 0 .751-3.44c-.179-1.784-1.175-3.361-2.803-4.44l-1.105 1.666c1.119.742 1.8 1.799 1.918 2.974a3.693 3.693 0 0 1-1.072 2.986l-1.192 1.192 1.618.475C18.951 13.701 19 17.957 19 18h2c0-1.789-.956-5.285-4.396-6.952z"></path>
+      <path d="M9.5 12c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2zm1.5 7H8c-3.309 0-6 2.691-6 6v1h2v-1c0-2.206 1.794-4 4-4h3c2.206 0 4 1.794 4 4v1h2v-1c0-3.309-2.691-6-6-6z"></path>
+    </svg>
+  );
+}
+
+function CompanyIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M19 2H9c-1.103 0-2 .897-2 2v6H5c-1.103 0-2 .897-2 2v9a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4c0-1.103-.897-2-2-2zM5 12h6v8H5v-8zm14 8h-6v-8c0-1.103-.897-2-2-2H9V4h10v16z"></path>
+      <path d="M11 6h2v2h-2zm4 0h2v2h-2zm0 4.031h2V12h-2zM15 14h2v2h-2zm-8 .001h2v2H7z"></path>
+    </svg>
+  );
+}
+
+function BenefitsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M4 21h9.62a3.995 3.995 0 0 0 3.037-1.397l5.102-5.952a1 1 0 0 0-.442-1.6l-1.968-.656a3.043 3.043 0 0 0-2.823.503l-3.185 2.547-.617-1.235A3.98 3.98 0 0 0 9.146 11H4c-1.103 0-2 .897-2 2v6c0 1.103.897 2 2 2zm0-8h5.146c.763 0 1.448.423 1.789 1.105l.447.895H7v2h6.014a.996.996 0 0 0 .442-.11l.003-.001.004-.002h.003l.002-.001h.004l.001-.001c.009.003.003-.001.003-.001.01 0 .002-.001.002-.001h.001l.002-.001.003-.001.002-.001.002-.001.003-.001.002-.001c.003 0 .001-.001.002-.001l.003-.002.002-.001.002-.001.003-.001.002-.001h.001l.002-.001h.001l.002-.001.002-.001c.009-.001.003-.001.003-.001l.002-.001a.915.915 0 0 0 .11-.078l4.146-3.317c.262-.208.623-.273.94-.167l.557.186-4.133 4.823a2.029 2.029 0 0 1-1.52.688H4v-6zM16 2h-.017c-.163.002-1.006.039-1.983.705-.951-.648-1.774-.7-1.968-.704L12.002 2h-.004c-.801 0-1.555.313-2.119.878C9.313 3.445 9 4.198 9 5s.313 1.555.861 2.104l3.414 3.586a1.006 1.006 0 0 0 1.45-.001l3.396-3.568C18.688 6.555 19 5.802 19 5s-.313-1.555-.878-2.121A2.978 2.978 0 0 0 16.002 2H16zm1 3c0 .267-.104.518-.311.725L14 8.55l-2.707-2.843C11.104 5.518 11 5.267 11 5s.104-.518.294-.708A.977.977 0 0 1 11.979 4c.025.001.502.032 1.067.485.081.065.163.139.247.222l.707.707.707-.707c.084-.083.166-.157.247-.222.529-.425.976-.478 1.052-.484a.987.987 0 0 1 .701.292c.189.189.293.44.293.707z"></path>
+    </svg>
+  );
+}
 
 export default function RouteNav() {
   const navLinks: NavLink[] = [
     {
       href: "/",
       text: "Home",
-      icon: "home",
+      icon: HomeIcon(),
     },
     {
       href: "/employees",
       text: "Employees",
-      icon: "people",
+      icon: PeopleIcon(),
     },
     {
       href: "/company",
       text: "Company",
-      icon: "business_center",
+      icon: CompanyIcon(),
     },
     {
       href: "/benefits",
       text: "Benefits",
-      icon: "heart",
+      icon: BenefitsIcon(),
     },
   ];
 
@@ -54,10 +112,12 @@ export default function RouteNav() {
           >
             <div
               className={
-                "h-14 w-14 rounded-full border-2 border-neutral-700 " +
+                "grid h-14 w-14 place-items-center rounded-full border-2 border-neutral-700 " +
                 (linkIsActive(link.href) && "bg-neutral-700")
               }
-            ></div>
+            >
+              {link.icon}
+            </div>
             {link.text}
           </Link>
         ))}
